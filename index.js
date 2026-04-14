ef25713960104544198edaf988f8a7a4aa394414
Zane <zimedua@gmail.com>
2026-04-13 23:20:55 -0500

    lab scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const burgers = ['Hamburger','Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+function addBurger(burger) 
+{ const newBurger = 'Flatburger';
+
+  burgers.push(newBurger);
+  if(true)
+    {
+       const anotherNewBurger = 'Maple Bacon Burger';
+         burgers.push(anotherNewBurger); 
+        
+    }
+    
+}
+ function changeFeaturedDrink() 
+    {
+       return featuredDrink = 'The JavaShake';
+    }
+        
